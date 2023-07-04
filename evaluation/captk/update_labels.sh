2e3ab6f6c24ca91f0d9e5bc325f17fd13a885678
verena <9377970+vpchung@users.noreply.github.com>
2023-07-03 22:05:21 -0700

    fix format; remove `verbose` from unzip and tar

--- evaluation/captk/update_labels.sh
+++ evaluation/captk/update_labels.sh
@@ -4,9 +4,10 @@ function captk () {
     /work/CaPTk/bin/Utilities "$@"
 }
 
+mkdir -p pred
 case $1 in
-    *.tar.gz) tar xzf  "$1" -C "pred" ;;
-    *.zip)    unzip -j "$1" -d "pred" ;;
+    *.tar.gz)  tar   -xf "$1" -C "pred" --transform='s/.*\///' ;;
+    *.zip)     unzip -jq "$1" -d "pred" ;;
 esac
 
 for f in pred/*.nii.gz
@@ -14,9 +15,9 @@ do
     filename=$(basename $f)
     label=${filename%.*.*}
     captk \
-        -i pred/${label}.nii.gz \
+        -i $f \
         -o updated-predictions/${label}.nii.gz \
         -cv 4,3
 done
 
-tar cvf predictions.tar.gz updated-predictions/*.nii.gz
+tar cf predictions.tar.gz updated-predictions/*.nii.gz
