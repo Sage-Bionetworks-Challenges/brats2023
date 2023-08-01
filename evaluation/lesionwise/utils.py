f6a7c03a8aae5418fdd23081fe7de5071e6f3eac
verena <9377970+vpchung@users.noreply.github.com>
2023-07-31 22:53:39 -0700

    update logic for utils

--- evaluation/lesionwise/utils.py
+++ evaluation/lesionwise/utils.py
@@ -4,18 +4,11 @@ import tarfile
 import zipfile
 
 
-def _is_hidden(member):
-    """Check whether file is hidden or not."""
-    hidden = ("__", "._", "~", ".DS_Store")
-    return os.path.split(member)[1].startswith(hidden)
-
-
 def _filter_tar(members, pattern):
     """Filter out directory name and hidden files in tarball."""
     files_to_extract = []
     for member in members:
-        if member.isfile() and not _is_hidden(member.name) and \
-            pattern in member.name:
+        if member.isfile() and pattern in member.name:
             files_to_extract.append(member)
     return files_to_extract
 
@@ -24,13 +17,12 @@ def _filter_zip(members, pattern):
     """Filter out directory name and hidden files in zip file."""
     files_to_extract = []
     for member in members:
-        if not member.is_dir() and not _is_hidden(member.filename) and \
-            pattern in member.filename:
+        if not member.is_dir() and pattern in member.filename:
             files_to_extract.append(member.filename)
     return files_to_extract
 
 
-def inspect_zip(f, unzip=True, path=".", pattern=""):
+def inspect_zip(f, unzip=True, path=".", pattern=".nii.gz"):
     """Untar or unzip file."""
     if zipfile.is_zipfile(f):
         with zipfile.ZipFile(f) as zip_ref:
