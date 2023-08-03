7a0b69b0f10e8504d9c5db72b5e2c5d0d584f015
verena <9377970+vpchung@users.noreply.github.com>
2023-08-03 15:40:39 -0700

    ignore .idea in archives

--- evaluation/captk/utils.py
+++ evaluation/captk/utils.py
@@ -4,11 +4,18 @@ import tarfile
 import zipfile
 
 
+def _is_hidden(member):
+    """Check whether file is hidden or not."""
+    hidden = ("__", "._", "~", ".DS_Store")
+    return os.path.split(member)[1].startswith(hidden) or ".idea" in member
+
+
 def _filter_tar(members, pattern):
     """Filter out directory name and hidden files in tarball."""
     files_to_extract = []
     for member in members:
-        if member.isfile() and pattern in member.name:
+        if member.isfile() and not _is_hidden(member.name) and \
+                pattern in member.name:
             files_to_extract.append(member)
     return files_to_extract
 
@@ -17,12 +24,13 @@ def _filter_zip(members, pattern):
     """Filter out directory name and hidden files in zip file."""
     files_to_extract = []
     for member in members:
-        if not member.is_dir() and pattern in member.filename:
+        if not member.is_dir() and not _is_hidden(member.filename) and \
+                pattern in member.filename:
             files_to_extract.append(member.filename)
     return files_to_extract
 
 
-def inspect_zip(f, unzip=True, path=".", pattern=".nii.gz"):
+def inspect_zip(f, unzip=True, path=".", pattern=""):
     """Untar or unzip file."""
     if zipfile.is_zipfile(f):
         with zipfile.ZipFile(f) as zip_ref:
