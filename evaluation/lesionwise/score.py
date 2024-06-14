272082c861606eb6bcfec270f029b6791daf6bf2
verena <9377970+vpchung@users.noreply.github.com>
2024-06-14 15:00:52 -0700

    remove lesionwise annots from BraTS-MEN submissions

--- evaluation/lesionwise/score.py
+++ evaluation/lesionwise/score.py
@@ -21,18 +21,16 @@ import metrics
 def get_args():
     """Set up command-line interface and get arguments."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--parent_id",
-                        type=str, required=True)
-    parser.add_argument("-s", "--synapse_config",
-                        type=str, default="/.synapseConfig")
-    parser.add_argument("-p", "--predictions_file",
-                        type=str, default="/predictions.zip")
-    parser.add_argument("-g", "--goldstandard_file",
-                        type=str, default="/goldstandard.zip")
-    parser.add_argument("-o", "--output",
-                        type=str, default="results.json")
-    parser.add_argument("-l", "--label",
-                        type=str, default="BraTS-GLI")
+    parser.add_argument("--parent_id", type=str, required=True)
+    parser.add_argument("-s", "--synapse_config", type=str, default="/.synapseConfig")
+    parser.add_argument(
+        "-p", "--predictions_file", type=str, default="/predictions.zip"
+    )
+    parser.add_argument(
+        "-g", "--goldstandard_file", type=str, default="/goldstandard.zip"
+    )
+    parser.add_argument("-o", "--output", type=str, default="results.json")
+    parser.add_argument("-l", "--label", type=str, default="BraTS-GLI")
     return parser.parse_args()
 
 
@@ -42,35 +40,39 @@ def calculate_per_lesion(pred, gold, label):
     goldstandard.
     """
     return metrics.get_LesionWiseResults(
-        pred_file=pred,
-        gt_file=gold,
-        challenge_name=label
+        pred_file=pred, gt_file=gold, challenge_name=label
     )
 
 
 def extract_metrics(df, label, scan_id):
     """Get scores for three regions: ET, WT, and TC."""
+    select_cols = [
+        "Labels",
+        "Legacy_Dice",
+        "Legacy_HD95",
+        "Sensitivity",
+        "Specificity",
+        "Num_TP",
+        "Num_FP",
+        "Num_FN",
+    ]
+    if label != "BraTS-MEN":  # FIXME after dryrunning
+        select_cols[1:1] = ["LesionWise_Score_Dice", "LesionWise_Score_HD95"]
     res = (
         df.set_index("Labels")
-        .filter(items=["Labels",
-                       "LesionWise_Score_Dice",
-                       "LesionWise_Score_HD95",
-                       "Legacy_Dice",
-                       "Legacy_HD95",
-                       "Sensitivity",
-                       "Specificity",
-                       "Num_TP", "Num_FP", "Num_FN"
-                       ])
+        .filter(items=select_cols)
         .filter(items=["ET", "WT", "TC"], axis=0)
         .reset_index()
         .assign(scan_id=f"{label}-{scan_id}")
         .pivot(index="scan_id", columns="Labels")
-        .rename(columns={
-            'LesionWise_Score_Dice': "LesionWise_Dice",
-            'LesionWise_Score_HD95': "LesionWise_Hausdorff95",
-            'Legacy_Dice': "Dice",
-            'Legacy_HD95': "Hausdorff95"
-        })
+        .rename(
+            columns={
+                "LesionWise_Score_Dice": "LesionWise_Dice",
+                "LesionWise_Score_HD95": "LesionWise_Hausdorff95",
+                "Legacy_Dice": "Dice",
+                "Legacy_HD95": "Hausdorff95",
+            }
+        )
     )
     res.columns = ["_".join(col).strip() for col in res.columns]
     return res
@@ -100,10 +102,11 @@ def main():
     # Get number of segmentations predicted by participant, as well as
     # descriptive statistics for results.
     cases_evaluated = len(results.index)
-    metrics = (results
-               .describe()
-               .rename(index={'25%': "25quantile", '50%': "median", '75%': "75quantile"})
-               .drop(["count", "min", "max"]))
+    metrics = (
+        results.describe()
+        .rename(index={"25%": "25quantile", "50%": "median", "75%": "75quantile"})
+        .drop(["count", "min", "max"])
+    )
     results = pd.concat([results, metrics])
 
     # CSV file of scores for all scans.
@@ -113,19 +116,24 @@ def main():
     # BraTS-MEN-RT organizers requested to only return full Dice
     # and HD95 scores back to participants.
     if args.label == "BraTS-MEN":  # FIXME after dryrunning
-        results.to_csv(
-            "all_scores.csv",
-            columns=[col for col in results.columns
-                     if not col.startswith("LesionWise") or col.startswith("Num")])
+        results.to_csv("all_scores.csv")
     else:
         results.to_csv(
             "all_scores.csv",
-            columns=[col for col in results.columns 
-                     if col.startswith("LesionWise") or col.startswith("Num")])
+            columns=[
+                col
+                for col in results.columns
+                if col.startswith("LesionWise") or col.startswith("Num")
+            ],
+        )
         results.to_csv(
             "all_full_scores.csv",
-            columns=[col for col in results.columns
-                     if not col.startswith("LesionWise") and not col.startswith("Num")])
+            columns=[
+                col
+                for col in results.columns
+                if not col.startswith("LesionWise") and not col.startswith("Num")
+            ],
+        )
         csv_full = synapseclient.File("all_full_scores.csv", parent=args.parent_id)
         csv_full = syn.store(csv_full)
 
@@ -134,10 +142,12 @@ def main():
 
     # Results file for annotations.
     with open(args.output, "w") as out:
-        res_dict = {**results.loc["mean"],
-                    "cases_evaluated": cases_evaluated,
-                    "submission_scores": csv.id,
-                    "submission_status": "SCORED"}
+        res_dict = {
+            **results.loc["mean"],
+            "cases_evaluated": cases_evaluated,
+            "submission_scores": csv.id,
+            "submission_status": "SCORED",
+        }
         if args.label != "BraTS-MEN":
             res_dict["submission_scores_legacy"] = csv_full.id
         res_dict = {k: v for k, v in res_dict.items() if not pd.isna(v)}
